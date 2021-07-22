4d900777bd18d45ab0cbe279f62452f813417684
Caio Macedo <53654561+LordeNCaio@users.noreply.github.com>
2021-07-22 12:44:47 -0300

    Finished App Front-End

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import ejs from 'ejs';
 import path from 'path'
+import { type } from 'os';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(path.resolve(), 'public')));
 app.set('views', [
     path.join(path.resolve(), '/views'),
@@ -17,11 +17,15 @@ app.set('views', [
 ])
 
 app.get('/', (req, res) => {
-    res.render('pages/index', { pageName: 'Portal' });
+
+    if(req.query.search == null) {
+        res.render('pages/index', { pageName: 'Portal' });
+    }
+    
 });
 
-app.get('/:type', (req, res) => {
-    res.send(req.params.type);
+app.get('/:topic', (req, res) => {    
+    res.render('pages/single-news', { pageName: req.params.topic.toUpperCase()  });
 });
 
 app.listen(5000, () => {
